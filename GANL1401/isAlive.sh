3765b89707e79113e3fd80222878bd4bda237fc0
Sabri RABAHIA <sabri.rabahia@ganil.fr>
2023-10-10 14:31:14 +0200

    get ip of the other machine is now a function

--- GANL1401/isAlive.sh
+++ GANL1401/isAlive.sh
@@ -4,20 +4,29 @@ source "configVar.shlib"
 
 echo "Pinging this machine interfaces and his Matching  one"
 
-linked=$(find /home/ubuntu22/code/transceivers -name "configVar.shlib" -type f -not -path "$(pwd)/*")
-echo "Other machine path: "$linked
+#Get IP of each interface
+# linked=$(find /home/ubuntu22/code/transceivers -name "configVar.shlib" -type f -not -path "$(pwd)/*")
+# echo "Other machine path: "$linked
 
-scopingList=$(sudo cat $linked | grep 'IP=('| awk -F'[=]' '{print $2}')
-echo "IP list to get : "$scopingList
+# scopingList=$(sudo cat $linked | grep 'IP=('| awk -F'[=]' '{print $2}')
+# echo "IP list to get : "$scopingList
 
-#Remove unwanted characters from the string-> ( ) "
-list_string="${scopingList//\(/}"
-list_string="${list_string//\)/}"
-list_string="${list_string//\"/}"
+# #Remove unwanted characters from the string-> ( ) "
+# list_string="${scopingList//\(/}"
+# list_string="${list_string//\)/}"
+# list_string="${list_string//\"/}"
 
-#Use IFS  env. var. to split the string into an array
-IFS=" " read -a trimedList <<< "$list_string"
-echo "Data in the usable array  ${trimedList[@]}"
+# #Use IFS  env. var. to split the string into an array
+# IFS=" " read -a trimedList <<< "$list_string"
+# echo "Data in the usable array  ${trimedList[@]}"
+
+#test avec function get_ips
+trimedList=($(get_ip))
+# Print the array of IP addresses
+echo "IP array: ${trimedList[@]}"
+
+
+
 
 #Get IP of each interface and find is Matching interface on the other machine to ping them 
 for ((i=0; i<${#GANL[@]}; i++))
--- GANL1401/perf.sh
+++ GANL1401/perf.sh
@@ -2,24 +2,10 @@
 source "../function.shlib"
 source "configVar.shlib"
 
-#todo: créer fonction GetIP qui vas chercher les IP de l'autre machine avec le bloc 
-# de code ci-dessous
 
-#Find the path to configVar of the second machine
-linked=$(find /home/ubuntu22/code/transceivers -name "configVar.shlib" -type f -not -path "$(pwd)/*")
-echo "Other machine path: "$linked
-
-scopingList=$(sudo cat $linked | grep 'IP=('| awk -F'[=]' '{print $2}')
-echo "IP list to get : "$scopingList
-
-#Remove unwanted characters from the string-> ( ) "
-list_string="${scopingList//\(/}"
-list_string="${list_string//\)/}"
-list_string="${list_string//\"/}"
-
-#Use IFS  env. var. to split the string into an array
-IFS=" " read -a trimedList <<< "$list_string"
-echo "Data in the usable array  ${trimedList[@]}"
+trimedList=($(get_ip))
+# Print the array of IP addresses
+echo "IP array: ${trimedList[@]}"
 
 #todo:Ajout du "Matcheur"
 #todo:trouver moyen pour créer tableau entre machine hote[KEY] et machine distante[VALUE]
@@ -28,10 +14,6 @@ ARRAY=( "1.86:1.21"
         "10.84:10.38"
         "11.86:11.34")
 
-#
-# echo $( sudo ssh $machineName$IP${trimedList[0]})
-# echo $(cd transceivers)
-# echo $(rm perfResult.txt)
 echo "Nom machine: "$machineName
 echo "IP machine: "$IP${trimedList[0]}
 echo "---Bandwith test of the interface---" > perfResult.txt 
@@ -41,9 +23,9 @@ echo $(cd transceivers)
 echo $(iperf -s | exit )
 '
 
-for animal in "${ARRAY[@]}" ; do
-    KEY="${animal%%:*}"
-    VALUE="${animal##*:}"
+for data in "${ARRAY[@]}" ; do
+    KEY="${data%%:*}"
+    VALUE="${data##*:}"
     echo "Bandwith test  in progress"
     echo "
     Testing from interface $IP$KEY to $IP$VALUE
